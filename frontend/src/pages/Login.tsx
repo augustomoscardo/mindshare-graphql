5bbbc9ba0f3a02aff76f62bb4c7b6652bd7669db
Augusto Moscardo <augustomsaluz@gmail.com>
2025-12-10 21:05:34 -0300

    criado páginas de login e signup

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import logo from "@/assets/logo.svg";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
+export function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  async function handleSubmit(event: React.FormEvent) { }
+
+  return (
+    <div className="flex flex-col min-h-[calc(100vh-4rem)] items-center justify-center gap-6">
+      <img src={logo} alt="" className="w-64 h-22" />
+
+      <Card className="w-full max-w-md rounded-xl">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">
+            Acesse a plataforma
+          </CardTitle>
+          <CardDescription>
+            Entre usando seu e-mail e senha cadastrados.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <label htmlFor="email">E-mail</label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="seu@email.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+
+            <div>
+              <label htmlFor="password">Senha</label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="********"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+
+            <Button
+              type="submit"
+              className="w-full "
+            >
+              Entrar
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+      <Card className="w-full max-w-md rounded-xl">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">
+            Ainda não tem uma conta?
+          </CardTitle>
+          <CardDescription>Cadastra-se agora mesmo</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button variant="outline" className="w-full" asChild>
+            <Link to="/signup">Criar conta</Link>
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
